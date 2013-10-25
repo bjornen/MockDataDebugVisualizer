1316f2247240f531f15edc4c9e11655918c1aa1e
bjornen <johan.m.nyman@gmail.com>
2013-10-25 16:01:15 +0200

    CB

---
+++ MockDataVisualizer/InitCodeDumper/CodeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MockDataDebugVisualizer.InitCodeDumper
+{
+    public class CodeBuilder
+    {
+        private readonly StringBuilder _codeBuilder;
+        private readonly List<string> _codeInitList;
+
+        public CodeBuilder()
+        {
+            _codeBuilder = new StringBuilder();
+            _codeInitList = new List<string>();
+        }
+
+        public void AddCode(string code)
+        {
+            _codeInitList.Add(code);
+        }
+
+        public void AddCode(List<string> codeInitList)
+        {
+            _codeInitList.AddRange(codeInitList);
+        }
+
+        public string Indentation(int level)
+        {
+            return new string('\t', level);
+        }
+    }
+}
